dffeff23e39c3b66aee762ddd7f8e6dcb09b6c9b
darkwebplayer <amrithanadhm@gmail.com>
2024-04-17 07:37:09 +0530

    added eslint for linting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-    '@nuxtjs/tailwindcss','nuxt-primevue','nuxt-icon'],
-   primevue: {
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue", "nuxt-icon", "@nuxt/eslint"],
+  primevue: {
     options: {
       ripple: true,
     },
     cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
   },
   css: ["primevue/resources/themes/lara-light-blue/theme.css"],
-     devtools: { enabled: true },
 
-})
+  devtools: { enabled: true },
+});
