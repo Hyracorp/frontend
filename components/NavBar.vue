2f3122a74e36f4e9d25ba723a224f6d035c42a9a
darkwebplayer <amrithanadhm@gmail.com>
2024-04-17 08:22:16 +0530

    image tag selfclosing fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,7 +5,7 @@
 
     <div class="w-screen p-5  flex justify-between items-center">
         <div class="">
-            <img src="/img/logo.png" alt="logo" class="w-40" />
+            <img src="/img/logo.png" alt="logo" class="w-40">
         </div>
         <div class="">
             <Button type="button" aria-haspopup="true" aria-controls="overlay_menu" class="md:hidden" text>
