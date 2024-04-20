e924681f2aa46a984eb8cd5365f4cee7b348d26f
darkwebplayer <amrithanadhm@gmail.com>
2024-04-20 11:57:24 +0530

    added pinia state management

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,17 @@
         <HomeFeaturedSection />
     </div>
 </template>
+<script setup lang="ts">
+import { useAuthStore } from '~/stores/auth';
+const auth = useAuthStore()
+const isAuthenticated = computed(() => auth.getAuthStatus)
+
+onMounted(async () => {
+    console.log(isAuthenticated.value)
+
+})
+async function login() {
+    await auth.login()
+    console.log(isAuthenticated.value)
+}
+</script>
