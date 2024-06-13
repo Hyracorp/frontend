1eb7f955247f4a3b2492acf89c25a545fa7ae4c6
darkwebplayer <amrithanadhm@gmail.com>
2024-06-13 19:20:39 +0530

    update auth to work with dynamic ids

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,23 @@
-
+function checkIfTruePath(paths,path) {
+    let isAllowed=false
+    paths.forEach(element => {
+        if(element==path){
+            isAllowed=true
+        }
+        if(path.startsWith(element)){
+            isAllowed=true
+        }
+    })
+    return isAllowed
+}
 export default defineNuxtRouteMiddleware(async(to, _from) => {
  
     if(import.meta.client){
-    const AllowedPaths = ['/login', '/signup','/reset-password','/']
-        console.log(!JSON.parse(localStorage.getItem("loginstatus")!) , !AllowedPaths.includes(to.path))
-    if(!JSON.parse(localStorage.getItem("loginstatus")!) || !AllowedPaths.includes(to.path)){
+    const AllowedPaths = ['/login', '/signup','/reset-password','/property']
+
+    const isAllowedPath = checkIfTruePath(AllowedPaths, to.path)
+   
+    if(!JSON.parse(localStorage.getItem("loginstatus")!) || !isAllowedPath ){
         
         return navigateTo('/login')
     }
