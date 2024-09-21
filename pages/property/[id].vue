4fbc55a680798477973f6ebea0c7206d4256e500
darkwebplayer <amrithanadhm@gmail.com>
2024-09-21 21:12:28 +0530

    custom og image on property disable

--- pages/property/[id].vue
+++ pages/property/[id].vue
@@ -26,16 +26,7 @@ onMounted(async () => {
     console.log(err);
   }
 });
-useSeoMeta({
-  title: "Hyracorp - Search Across 1000s of Properties",
-  ogTitle: "Hyracorp Property Search",
-  description: "Search across 10000+ verified property listings",
-  ogDescription: "Search across 10000+ verified property listings",
-  ogImage: property.value?.photos[0]?.photo_url
-    ? `${cloudinary_base_url.value}${property.value?.photos[0]?.photo_url}`
-    : "https://app.hyracorp.com/img/og.jpeg",
-  twitterCard: "summary_large_image",
-});
+
 definePageMeta({
   layout: "default",
   title: "Property Details",
